4c9b2bdb40b4f7398eac79d3b9f8092afd746c47
Michael Burt <burtrico@gmail.com>
2021-07-11 22:49:01 -0500

    Professional Updates Rev 1

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+//h2.textContent = "This content added by JavaScript"; 
 
 document.querySelector('body').appendChild(h2); 
+
+const para1 = document.getElementById("p1");
+const para2 = document.getElementById("p2");
+
+document.querySelector('body').appendChild(h2); 
+
+
+const a1 = document.createElement('a');
+link1 = document.createTextNode("burtrico")
+a1.appendChild(link1);
+a1.title = "This is a link to my github profile"; 
+a1.href = "https://github.com/burtrico"; 
+a1.id = "a1"
+document.body.appendChild(a1); 
+
+document.write("<br>");
+
+const a2 = document.createElement('a');
+link2 = document.createTextNode("@burtrico")
+a2.appendChild(link2);
+a2.title = "This is a link to my twitter profile"; 
+a2.href = "https://twitter.com/burtrico"; 
+a2.id = "a2";
+document.body.appendChild(a2); 
+
+//const p1 = document.createElement("p");
+//p1.className = "und";
+const text1 = document.createTextNode('Github: ');
+const child1 = document.getElementById('a1');
+child1.parentNode.insertBefore(text1, child1);
+
+const text2 = document.createTextNode('Twitter: ');
+const child2 = document.getElementById('a2');
+child2.parentNode.insertBefore(text2, child2);
+
+
 
